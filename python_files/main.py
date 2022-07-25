693f788aab3f1cb7f5ac3ad8bc4b7f0379017762
Eli-Drew <drew313@live.missouristate.edu>
2022-07-24 23:50:42 -0500

    Made a menu. Implemented screen manager for multiple screens.

--- python_files/main.py
+++ python_files/main.py
@@ -13,18 +13,31 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
-class GameBoard(GridLayout):
+class GameBoard(Screen):
     # create_board() might only need this for backend logic purposes. or just make an array.
     # move()
     pass
 
-class GameWindow(GridLayout):
+class GameWindow(Screen):
     # game_board = ObjectProperty(None)
     pass
 
+class MainWindow(Screen):
+    pass
+
+class WindowManger(ScreenManager):
+    pass
+#=====================================================================
+
+kv = Builder.load_file("SixteenCounter.kv")
+# kv = Builder.load_file("new_window.kv")
+
 class SixteenCounterApp(App):
     # seems like this part will act as my 'main()'
-    pass
+    def build(self):
+        return kv
 
 SixteenCounterApp().run()
